bfc41ac4ee1f730c8b104a0d3fc69a3be29010a6
Stuart Moore <stuartjmoore@gmail.com>
2012-08-10 18:00:27 -0400

    Initial Commit

---
+++ PlayBar/SMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SMAppDelegate.h
+//  PlayBar
+//
+//  Created by Stuart Moore on 8/10/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface SMAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ PlayBar/SMAppDelegate.m
@@ -0,0 +1,20 @@
+//
+//  SMAppDelegate.m
+//  PlayBar
+//
+//  Created by Stuart Moore on 8/10/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "SMAppDelegate.h"
+
+@implementation SMAppDelegate
+
+@synthesize window = _window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
